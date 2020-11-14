e4122c2d53a98540c7fe5a393f60595ece39e453
Gabriel Williams <Gabriel_Williams@ranken.org>
2020-11-14 17:39:01 -0600

    ch4-5labs javascript

--- labs/JS_CH04_LAB/sales_tax/sales_tax.js
+++ labs/JS_CH04_LAB/sales_tax/sales_tax.js
@@ -1,12 +1,12 @@
-var $ = function(id)
+let $ = function(id)
 {
   return document.getElementById(id);
 }
 
-var processEntries = function()
+let processEntries = function()
 {
-  var inpSubTotal = parseFloat($("subtotal").value);
-  var inpTaxRate = parseFloat($("tax_rate").value);
+  let inpSubTotal = parseFloat($("subtotal").value);
+  let inpTaxRate = parseFloat($("tax_rate").value);
   if (inpSubTotal <= 0 || subtotal > 10000)
   {
     $("subTotalsMessage").innerHTML = "Subtotal must be > 0 and < 10000";
@@ -17,16 +17,16 @@ var processEntries = function()
     $("taxRateMsg").innerHTML = "Tax rate must be > 0 and < 12";
   }
 
-  var calTax = inpSubTotal * (inpTaxRate / 100);
+  let calTax = inpSubTotal * (inpTaxRate / 100);
   calTax = parseFloat(calTax.toFixed(2));
-  var totAmt = inpSubTotal + calTax;
+  let totAmt = inpSubTotal + calTax;
 
   $("sales_tax").value = calTax;
 
   $("total").value = totAmt.toFixed(2);
 }
 
-var clear_click = function()
+let clear_click = function()
 {
   $("subtotal").value = "";
   $("tax_rate").value = "";
